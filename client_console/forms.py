58be8842cde585d883001e885be0ae6a4771a534
mohanish <mpgadge@gmail.com>
2019-01-08 11:01:33 +0530

    New UI added with business logic- client_console

---
+++ client_console/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+class ClientConsoleForm(forms.Form):
+    text_to_convert = forms.CharField(label="Enter Text to Convert",widget=forms.Textarea(attrs={'class':'input'}))
+    src_langauge    = forms.ChoiceField(
+                                    label="Select Source Language",
+                                    choices=[('SL', 'select'),('en', 'English'), ('hi', 'Hindi'), ('ja', 'Japanese'), ('ko', 'Korean')],
+                                    widget=forms.Select(attrs={'class': 'input'}))
+    dest_langauge   = forms.ChoiceField(
+                                    label="Select Destination Language",
+                                    choices=[('SL', 'select'),('en', 'English'),('hi', 'Hindi'), ('ja', 'Japanese'),('ko', 'Korean')],
+                                    widget=forms.Select(attrs={'class':'input'}))
+
+
+
+
+    ''' 
+    1.Readonly Field:
+    converted_text=forms.CharField(label="Translated Text",
+                                   required=False,
+                                   widget=forms.TextInput(attrs={'class':'input','readonly':'true'}))
+    '''
+
+
+
+
+
+
--- client_console/views.py
+++ client_console/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-
+from .forms import ClientConsoleForm
+from googletrans import Translator
 
 def home(request):
-   return render(request,'client_console/home.html',{})
+    form = ClientConsoleForm()
+    context = {
+        'form': form
+    }
+    if request.method == 'POST':
+        form = ClientConsoleForm(request.POST)
+
+        if form.is_valid():
+            text_to_convert = form.cleaned_data['text_to_convert']
+            src_langauge = form.cleaned_data['src_langauge']
+            dest_langauge = form.cleaned_data['dest_langauge']
+
+            translator = Translator()
+            translated_obj=translator.translate(text_to_convert,dest=dest_langauge,src=src_langauge)
+            #print(translated_obj.text)
+            form.cleaned_data['converted_text']=translated_obj.text
+            context.update({'translated_obj':translated_obj})
+            print(form.cleaned_data['converted_text'])
+
+
+
+    return render(request,'client_console/home.html',context)
